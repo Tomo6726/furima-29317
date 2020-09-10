7d37df3c01546249aff92dec6bba29ef243e6d10
Tomo6726 <69682334+Tomo6726@users.noreply.github.com>
2020-09-10 18:28:11 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence:  true
+  validates :nickname,  presence:  true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: " Include both letters and numbers"}
+  validates :birth_day, presence: true
+  # validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"},presence:  true
+  # validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"},presence:  true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}, presence:  true
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: " Full-width katakana characters"}, presence:  true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"} do
+    validates :first_name
+    validates :last_name
+  end
+
 end
+# message: "is too short (minimum is 6 characters)" 
